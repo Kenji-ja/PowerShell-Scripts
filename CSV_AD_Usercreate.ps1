8331c5860d221e73b31af6ebcfbe9d8b40a3daf2
Kenji-ja <148914663+Kenji-ja@users.noreply.github.com>
2023-12-06 19:23:15 -0600

    Add files via upload

---
+++ CSV_AD_Usercreate.ps1
@@ -0,0 +1,30 @@
+﻿#Import AD moddule
+Import-Module ActiveDirectory
+
+#Custom CSV header
+$Header = "Fname" , "Lname"
+
+#Default password for accounts
+$Password = "P@ssword"
+
+#set path for the ddirectory
+$path = "changeme"
+
+#Import CSV with custom header, then format information and apss it to New-AD user
+Import-Csv -Path $UsersFilePath -Header $Header | ForEach {
+#Create variables
+$giveName = ($_.Fname + " " + $_.Lname)
+$Fname = $_.Fname
+$Lname = $_.Lname
+
+#Create AD user
+New-ADUser `
+   -Name $Fame `
+   -GivenName $givenName `
+   -Surname $Lname `
+   -UserPrincipalName "$Fname $Lname" `
+   -AccountPassword (ConvertTo-SecureString "$Password" -AsPlainText -Force) `
+   -Path "$path" `
+   -ChangePasswordAtLogon $true `
+   -Enabled $true
+}
