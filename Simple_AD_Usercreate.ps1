a00693fde7d9bca202dbf1eb410ad5da8715f47e
Kenji-ja <148914663+Kenji-ja@users.noreply.github.com>
2023-12-06 19:00:21 -0600

    Add files via upload

---
+++ Simple_AD_Usercreate.ps1
@@ -0,0 +1,13 @@
+﻿#Import AD moddule
+Import-Module ActiveDirectory
+
+#Create AD user
+New-ADUser `
+   -Name $name `
+   -GivenName $givenname `
+   -Surname $lname `
+   -UserPrincipalName "$name.$lname" `
+   -AccountPassword (ConvertTo-SecureString "$password" -AsPlainText -Force) `
+   -Path "$path" `
+   -ChangePasswordAtLogon $true `
+   -Enabled $true
