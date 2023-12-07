2de7aeebb9c495e3728dd4838b6254d7aefa02a3
Kenji-ja <148914663+Kenji-ja@users.noreply.github.com>
2023-12-06 19:33:10 -0600

    Add files via upload

---
+++ Prompt_AD_Usercreate.ps1
@@ -0,0 +1,30 @@
+﻿#Import AD moddule
+Import-Module ActiveDirectory
+
+#Default password for accounts
+$Password = Read-Host -Prompt "Enter teh default password for all accounts (will be changed upon login): "
+
+#set path for the ddirectory
+$path = Read-Host -Prompt "Enter the distinguished name of the directory: "
+
+#Import CSV with custom header, then format information and apss it to New-AD user
+Import-Csv -Path $UsersFilePath -Header $Header | ForEach {
+#Create variables
+$Fname = Read-Host -Prompt "Enter the first name"
+$Lname = Read-Host -Prompt "Enter the Last name"
+$giveName = ($_.Fname + " " + $_.Lname)
+
+#Create AD user
+New-ADUser `
+   -Name $Fname `
+   -GivenName $givenName `
+   -Surname $Lname `
+   -UserPrincipalName "$Fname $Lname" `
+   -AccountPassword (ConvertTo-SecureString "$Password" -AsPlainText -Force) `
+   -Path "$path" `
+   -ChangePasswordAtLogon $true `
+   -Enabled $true
+
+#FeedBack
+Write-Host "created" $givenName
+}
