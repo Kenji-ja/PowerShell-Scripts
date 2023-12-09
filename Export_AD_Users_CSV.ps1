1e4eb624fb3792da9ac3aacbfca3c69e7fb6a1a3
Kenji-ja <148914663+Kenji-ja@users.noreply.github.com>
2023-12-09 17:37:41 -0600

    Add files via upload

--- Export_AD_Users_CSV.ps1
+++ Export_AD_Users_CSV.ps1
@@ -3,8 +3,16 @@ Import ActiveDirectory
 
 #Path for the OU
 $OUpath = Read-Host -Prompt "Enter the the distinguished name of the OU: "
-#Path that teh CSV file will be saved
-$ExportPath = "C:\ADusers.csv"
-
+#Path that the CSV file will be saved
+$ExportPath = Read-Host -Prompt "Enter the the path for where the file will be saved (Ex: C:\Users.csv): "
+#Empty array for users
+$ADUsers = @()
 #get AD users from OU, filter object properties, then export to CSV
-Get-ADUser -Filter * -SearchBase $OUpath | Select-Object DistinguishedName,Name,UserPrinciplaName,SamAccountName,Surname | Export-Csv -NoTypeInformation $ExportPath
+$ADUsers += Get-ADUser -Filter * -SearchBase $OUpath -Properties 
+
+$ADUsers | Sort-Object Name | Select-Object `
+@{Label = "GivenName"; Expression = {$_.GivenName}},
+@{Label = "Surname"; Expression = {$_.Surname}},
+@{Label = "SamAccountName"; Expression = {$_.SamAccountName}},
+@{Label = "UserPrincipalName"; Expression = {$_.UserPrincipalName}},
+@{Label = "Name"; Expression = {$_.Name}} | Export-Csv -NoTypeInformation $ExportPath
