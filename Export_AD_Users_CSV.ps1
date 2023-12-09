30cce781be112cb6116faab91d68e1000b2b715c
Kenji-ja <148914663+Kenji-ja@users.noreply.github.com>
2023-12-08 19:11:40 -0600

    Add files via upload

---
+++ Export_AD_Users_CSV.ps1
@@ -0,0 +1,10 @@
+﻿#Import AD library
+Import ActiveDirectory
+
+#Path for the OU
+$OUpath = Read-Host -Prompt "Enter the the distinguished name of the OU: "
+#Path that teh CSV file will be saved
+$ExportPath = "C:\ADusers.csv"
+
+#get AD users from OU, filter object properties, then export to CSV
+Get-ADUser -Filter * -SearchBase $OUpath | Select-Object DistinguishedName,Name,UserPrinciplaName,SamAccountName,Surname | Export-Csv -NoTypeInformation $ExportPath
